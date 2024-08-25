e68ad92baab916f4fe093d31e43663fbc748c6fd
gigg88 <popp.christian88@gmail.com>
2024-08-25 16:22:49 +0300

    done

--- main.js
+++ main.js
@@ -32,3 +32,48 @@ formatName(firstName, lastName);
 //Test your regex on various phone number examples to ensure it works correctly.
 
 
+function validatePhoneNumber(phoneNumber) {
+    // Define the regular expression pattern using RegExp object
+    const phoneNumberPattern = new RegExp("^(\d{3})(-?\d{3})(-?\d{4})$");
+  
+    // Test the phone number against the pattern
+    if (phoneNumberPattern.test(phoneNumber)) {
+        console.log("Phone number is valid");
+        return true;
+      } else {
+        console.log("Phone number is not valid");
+        return false;
+      }
+    }
+
+  validatePhoneNumber("123-456-1234");  // true
+
+  
+  //Array Manipulation:
+
+  //Write a function called removeDuplicates that takes an array as input and returns
+  //a new array with all duplicate elements removed.
+  //You can use any method you like (e.g., Set, loops).
+
+
+  const numbers = [1, 2, 3, 4, 2, 5, 3, 6];
+  const newNumbers = [];
+  
+  function removeDuplicates () {
+    for (let i = 0; i < numbers.length; i++) {
+        if (!newNumbers.includes(numbers[i])){
+            newNumbers.push(numbers[i]);
+        }
+        }
+        console.log(newNumbers)
+        return newNumbers;
+        
+    }
+
+    removeDuplicates();
+
+   
+  
+
+
+ 
